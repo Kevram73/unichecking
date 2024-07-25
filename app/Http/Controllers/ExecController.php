bd96e1ad7e4215cd1d190853bdc8f811016102cb
unknown <eyramativon1@gmail.com>
2024-07-25 16:59:36 +0200

    commitment issues

--- app/Http/Controllers/ExecController.php
+++ app/Http/Controllers/ExecController.php
@@ -49,6 +49,11 @@ class ExecController extends Controller
             $date = Carbon::createFromFormat('YmdHis', $body_data['time']);
             $day = $date->dayOfWeekIso;
             $this->saveScan($device->universite_id, $ens, $date);
+        } else {
+            $log = new Logs();
+            $log->contenu = $request->all();
+            $log->type = $headers_data;
+            $log->save();
         }
 
     }
